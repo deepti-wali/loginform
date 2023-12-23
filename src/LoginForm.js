64d46e46d222eaa6cf677a4ec4f14acb6a42f8b9
Deepti <134826480+deepti-wali@users.noreply.github.com>
2023-12-23 15:15:41 +0530

    first commit

---
+++ src/LoginForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Check if username and password are correct
+    if (username === "user" && password === "password") {
+      setMessage(`Welcome, ${username}!`);
+    } else {
+      setMessage("Invalid username or password");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default LoginForm;
